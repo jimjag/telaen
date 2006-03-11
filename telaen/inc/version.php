f37a8069dd82f0ce734cf38c0e194e2db406b47b
Jim Jagielski <jimjag@gmail.com>
2006-03-11 15:20:35 +0000

    And back to dev version after tagging TELAEN_1_1_0_B1

--- telaen/inc/version.php
+++ telaen/inc/version.php
@@ -14,7 +14,7 @@ Telaen is based on Uebimiau (http://uebimiau.sourceforge.net)
 # "X-Mailer" field, footer
 ########################################################################
 
-$appversion = "1.1.0-beta1";
+$appversion = "1.1.0-dev";
 $appname = "Telaen Webmail";
 
 ?>
