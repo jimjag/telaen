62273d58ee064615d03334adaafec7682c93350e
Jim Jagielski <jimjag@gmail.com>
2006-03-17 16:21:06 +0000

    Add in hungi.mozilla "classic" theme

---
+++ telaen/themes/hungi.mozilla/webmail.js
@@ -0,0 +1,21 @@
+<!--
+	function mOvr(src,clrOver) {
+		if (!src.contains(event.fromElement)) {
+			src.style.cursor = 'hand';
+			src.style.background = 'url(themes/hungi.mozilla/menu-bg2.gif)';
+			src.bgColor = clrOver;
+		}
+	}
+	function mOut(src,clrIn) {
+		if (!src.contains(event.toElement)) {
+			src.style.cursor = 'default';
+			src.style.background = '';
+			src.bgColor = clrIn;
+		}
+	}
+	function mClk(src) {
+		if(event.srcElement.tagName=='TD'){
+			src.children.tags('A')[0].click();
+		}
+	}
+// -->	
