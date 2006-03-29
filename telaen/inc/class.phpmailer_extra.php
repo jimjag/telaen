ba9426f45af84b010e9893445c882203f823ab29
Jim Jagielski <jimjag@gmail.com>
2006-03-29 16:03:52 +0000

    PHPMailer has a bug where, if using the 'mail' Mailer, CC addresses where not being appended to the headers!

--- telaen/inc/class.phpmailer_extra.php
+++ telaen/inc/class.phpmailer_extra.php
@@ -34,14 +34,20 @@ class PHPMailer_extra extends PHPMailer {
         // In PHPMailer, when we use the 'mail' Mailer type,
         // the To and Subject lines aren't added to the header,
         // thus we don't see them in the SENT folder. So, in
-        // this case, force them in
+        // this case, force them in. Also, some other headers
+        // aren't correct, so fix 'em
         if ($this->Mailer == "mail") {
             if (count($this->to) > 0)
                 $fheader .= $this->AddrAppend("To", $this->to);
             else
                 $fheader .= $this->HeaderLine("To", "undisclosed-recipients:;");
-            $fheader .= $this->HeaderLine("Subject", $this->EncodeHeader(trim($this->Subject)));
 
+            // Add in the Subject and CC lines
+            $fheader .= $this->HeaderLine("Subject", $this->EncodeHeader(trim($this->Subject)));
+            if(count($this->cc) > 0) {
+                $fheader .= $this->AddrAppend("Cc", $this->cc);
+                $header .= $this->AddrAppend("Cc", $this->cc);
+            }
         }
         $this->FormattedMail = sprintf("%s\r\n\r\n%s",$fheader,$body);
 
