77092a196e4123ba076ab2192962459024f06a1e
Andrea Volani <andreavola@gmail.com>
2006-05-04 14:16:11 +0000

    Solved body alignment problem in IE, menu highlight javascript adjusted for FF compatibility

--- telaen/themes/default/webmail.js
+++ telaen/themes/default/webmail.js
@@ -1,19 +1,14 @@
 <!--
 	function mOvr(src,clrOver) {
-		if (!src.contains(event.fromElement)) {
-			src.style.cursor = 'hand';
-			src.bgColor = clrOver;
-		}
+		src.style.cursor = 'pointer';
+		src.style.backgroundColor = clrOver;
+		
 	}
 	function mOut(src,clrIn) {
-		if (!src.contains(event.toElement)) {
-			src.style.cursor = 'default';
-			src.bgColor = clrIn;
-		}
+		src.style.cursor = 'default';
+		src.style.backgroundColor = clrIn;
 	}
-	function mClk(src) {
-		if(event.srcElement.tagName=='TD'){
-			src.children.tags('A')[0].click();
-		}
-	}
+	//function mClk(src) {
+	//	src.getElementsByTagName('a')[0].click();
+	//}
 // -->	
