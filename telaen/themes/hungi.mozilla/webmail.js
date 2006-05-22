435277220297d98ae32130723711c38b78427c60
Andrea Volani <andreavola@gmail.com>
2006-05-22 07:38:18 +0000

    Improved titles alignment and spam message moved on top and linked

--- telaen/themes/hungi.mozilla/webmail.js
+++ telaen/themes/hungi.mozilla/webmail.js
@@ -1,21 +1,15 @@
 <!--
 	function mOvr(src,clrOver) {
-		if (!src.contains(event.fromElement)) {
-			src.style.cursor = 'hand';
-			src.style.background = 'url(themes/hungi.mozilla/images/menu-bg2.gif)';
-			src.bgColor = clrOver;
-		}
+		src.style.cursor = 'pointer';
+		src.style.backgroundImage = 'url(themes/hungi.mozilla/images/menu-bg2.gif)';
+		src.style.backgroundColor = clrOver;	
 	}
 	function mOut(src,clrIn) {
-		if (!src.contains(event.toElement)) {
-			src.style.cursor = 'default';
-			src.style.background = '';
-			src.bgColor = clrIn;
-		}
+		src.style.cursor = 'default';
+		src.style.backgroundImage = '';
+		src.style.backgroundColor = clrIn;
 	}
 	function mClk(src) {
-		if(event.srcElement.tagName=='TD'){
-			src.children.tags('A')[0].click();
-		}
+		src.children.tags('A')[0].click();
 	}
 // -->	
