f45ec2cec51ebef76d563c7ec6439c91e93772ea
Andrea Volani <andreavola@gmail.com>
2006-08-30 18:26:50 +0000

    version update

--- telaen/inc/version.php
+++ telaen/inc/version.php
@@ -14,7 +14,7 @@ Telaen is based on Uebimiau (http://uebimiau.sourceforge.net)
 # "X-Mailer" field, footer
 ########################################################################
 
-$appversion = "1.1.1-dev";
+$appversion = "1.1.1-rc1;
 $appname = "Telaen Webmail";
 
 ?>
