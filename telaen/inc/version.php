24beefdee8b2dd7c20e9c69c591e352a0dd2a9ca
Jim Jagielski <jimjag@gmail.com>
2007-02-02 23:33:07 +0000

    Get ready for rc1 release... note check_install.php fix

--- telaen/inc/version.php
+++ telaen/inc/version.php
@@ -14,7 +14,7 @@ Telaen is based on Uebimiau (http://uebimiau.sourceforge.net)
 # "X-Mailer" field, footer
 ########################################################################
 
-$appversion = "1.1.3";
+$appversion = "1.1.3-rc1";
 $appname = "Telaen Webmail";
 
 ?>
