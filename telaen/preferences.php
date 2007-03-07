7697f95036aa109fc9c7c95853e9f254ffcaeba4
Andrea Volani <andreavola@gmail.com>
2007-03-07 15:50:40 +0000

    First filters approach in preferences

--- telaen/preferences.php
+++ telaen/preferences.php
@@ -7,35 +7,8 @@ Telaen is a GPL'ed software developed by
 
 *************************************************************************/
 
-
 require("./inc/inc.php");
 require("./folder_list.php");
-
-
-
-if(isset($f_real_name)) {
-	$myprefs["real-name"]		= $f_real_name;
-	$myprefs["reply-to"]		= $f_reply_to;
-	$myprefs["save-to-trash"]	= $f_save_trash;
-	$myprefs["st-only-read"]	= $f_st_only_read;
-	$myprefs["empty-trash"]		= $f_empty_on_exit;
-	$myprefs["empty-spam"]		= $f_empty_spam_on_exit;
-	$myprefs["unmark-read"]		= $f_unmark_read_on_exit;
-	$myprefs["save-to-sent"]	= $f_save_sent;
-	$myprefs["rpp"]			= $f_rpp;
-	$myprefs["add-sig"]		= $f_add_sig;
-	$myprefs["signature"]   	= $f_sig;
-	$myprefs["timezone"]		= $f_timezone;
-	$myprefs["display-images"]	= $f_display_images;
-	$myprefs["editor-mode"]		= $f_editor_mode;
-	$myprefs["refresh-time"]	= $f_refresh_time;
-	$myprefs["first-login"] 	= 1;
-	$myprefs["spamlevel"]		= $f_spamlevel;
-	$myprefs["version"]		= $appversion;
-	save_prefs($myprefs); unset($myprefs);
-}
-
-$prefs = load_prefs();
 
 $jssource = "
 
@@ -47,21 +20,99 @@ function folderlist() { location = 'folders.php?folder=".urlencode($folder)."'}
 function goend() { location = 'logout.php'; }
 function goinbox() { location = 'messages.php?folder=inbox'; }
 function search() { location = 'search.php'; }
-function emptytrash() {	location = 'folders.php?empty=trash&folder=".urlencode($folder)."&goback=true';}
+function emptytrash() { location = 'folders.php?empty=trash&folder=".urlencode($folder)."&goback=true';}
 function addresses() { location = 'addressbook.php'; }
 function dis() { 
-	with(document.forms[0]) { 
-		f_st_only_read.disabled = !f_save_trash.checked; 
-		if(f_st_only_read.checked) f_st_only_read.checked = f_save_trash.checked; 
-		disbl = !f_save_trash.checked
-	} 
+        with(document.forms[0]) { 
+                f_st_only_read.disabled = !f_save_trash.checked; 
+                if(f_st_only_read.checked) f_st_only_read.checked = f_save_trash.checked; 
+                disbl = !f_save_trash.checked
+        } 
 }
 function checkDis() { if (disbl) return false; }
 //]]>
 </script>
 ";
 
-$smarty->assign("umJS",$jssource);
+$smarty->assign("umJS", $jssource);
+
+// load filters
+$filename = $userfolder."_infos/filters.ucf";
+$myfile = $UM->_read_file($filename);
+$filters = Array();
+
+if($myfile != "")  
+        $filters = unserialize(base64_decode($myfile));
+
+// we have to do something...
+if(isset($_POST['action'])) {
+        $action = $_POST['action'];
+        switch ($action) {
+                
+		case "savePrefs":
+			// pick the new settings and save			
+			$myprefs["real-name"]           = $_POST['f_real_name'];
+		        $myprefs["reply-to"]            = $_POST['f_reply_to'];
+		        $myprefs["save-to-trash"]       = $_POST['f_save_trash'];
+		        $myprefs["st-only-read"]        = $_POST['f_st_only_read'];
+		        $myprefs["empty-trash"]         = $_POST['f_empty_on_exit'];
+		        $myprefs["empty-spam"]          = $_POST['f_empty_spam_on_exit'];
+		        $myprefs["unmark-read"]         = $_POST['f_unmark_read_on_exit'];
+		        $myprefs["save-to-sent"]        = $_POST['f_save_sent'];
+		        $myprefs["rpp"]                 = $_POST['f_rpp'];
+		        $myprefs["add-sig"]             = $_POST['f_add_sig'];
+		        $myprefs["signature"]           = $_POST['f_sig'];
+		        $myprefs["timezone"]            = $_POST['f_timezone'];
+		        $myprefs["display-images"]      = $_POST['f_display_images'];
+		        $myprefs["editor-mode"]         = $_POST['f_editor_mode'];
+		        $myprefs["refresh-time"]        = $_POST['f_refresh_time'];
+		        $myprefs["first-login"]         = 1;
+		        $myprefs["spamlevel"]           = $_POST['f_spamlevel'];
+		        $myprefs["version"]             = $appversion;
+		        save_prefs($myprefs);
+			unset($myprefs);
+
+			break;
+
+		case "addFilter":
+            
+			// check for params
+                        if(!isset($_POST['filterType']) ||
+                                !isset($_POST['filterField']) ||
+                                !isset($_POST['filterMatch']) ||
+                                !isset($_POST['filterType']))
+                                break;
+
+                        // set the folder only for move
+                        $destFolder = "";
+                        if(intval($_POST['filterType']) == 1) {
+                                $destFolder = trim($_POST['filterDestFolder']);
+                        }
+
+                        // add the filter
+                        $filters[] =  Array(
+		                "type"          => intval($_POST['filterType']),
+        		        "field"         => intval($_POST['filterField']),
+                		"match"         => trim($_POST['filterMatch']),
+	                	"moveto"        => $destFolder
+        	        );
+
+			// save the file
+		        $content = base64_encode(serialize($filters));
+		        $UM->_save_file($filename, $content);
+
+                        break;
+
+		case "delFilter":
+
+                        break;
+        }
+}
+
+$smarty->assign("filterList", $filters);
+
+// load prefs
+$prefs = load_prefs();
 
 $aval_rpp = Array(10,20,30,40,50,100,200);
 $sel_rpp = "<select name=\"f_rpp\">\r";
@@ -123,7 +174,7 @@ $smarty->assign("umSignature",$txtsignature);
 $smarty->assign("umTimezoneSelect",$tzselect);
 
 $smarty->assign("umSpamLevelVals", array(0,1,2,3,4,5,6,7,8,9));
-$smarty->assign("umSpamLevel",$prefs["spamlevel"]);
+$smarty->assign("umSpamLevel", $prefs["spamlevel"]);
 
 $smarty->display("$selected_theme/preferences.htm");
 
