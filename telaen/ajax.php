c20e86ae5acb8e28a26fca1d46af408fea18d081
Andrea Volani <andreavola@gmail.com>
2007-04-10 14:42:43 +0000

    Correct use of server vars in redir and ajax receipt send function (actually not tested)

--- telaen/ajax.php
+++ telaen/ajax.php
@@ -14,57 +14,101 @@ Telaen is a GPL'ed software developed by
 session_name('telaen');
 session_start();
 
+require("./inc/lib.php");
+require("./inc/version.php");
+
+// common session check, exit if no auth
+$SS = new Session();
+$sess = $SS->Load();
+
+if(!$sess["auth"]) {
+	echo "error: your session seems expired";
+	die();
+}
+
 // cheking for main param
 if(isset($_POST['action'])) {
 	
-	// config & session
+	// config
 	require("./inc/config.php");
-	require("./inc/lib.php");
-	
+
+	$prefs = load_prefs();
+		
 	$action = $_POST['action'];
 	switch($action) {
 	
+		// send a read receipt
 		case "sendReceipt":
-			sendReceipt();									
+			if(!isset($_POST['recipient']))
+				break;
+
+			$recipient = $_POST['recipient'];			
+			$receiptSubj = $_POST['receipt_subj'];
+			$receiptMsg = $_POST['receipt_msg'];
+
+			require("./inc/class.phpmailer.php");
+		        require("./inc/class.phpmailer_extra.php");
+
+			// init mailer
+		        $mail = new PHPMailer_extra;
+		       	$mail->PluginDir = "./inc/";
+			$mail->SetLanguage("en","langs/");
+			$mail->CharSet = $default_char_set;
+			$mail->Hostname = getenv("SERVER_NAME");
+			$mail->Host = $smtp_server;
+			$mail->WordWrap = 76;
+			$mail->Priority = 3;
+			$mail->SMTPDebug = false;
+			$mail->Mailer = $mailer_type;
+			if ($phpmailer_sendmail != "") {
+				$mail->Sendmail = $phpmailer_sendmail;
+			}
+			if ($phpmailer_timeout != 0) {
+				$mail->Timeout = $phpmailer_timeout;
+        		}
+
+			// for password authenticated servers
+			$mail->SMTPAuth = $use_password_for_smtp;
+			$mail->Username = $sess["user"];
+			$mail->Password = $sess["pass"];
+
+			// build the email
+			$mail->From = ($allow_modified_from && !empty($prefs["reply-to"]))?$prefs["reply-to"]:$sess["email"];						
+			$mail->FromName = $UM->mime_encode_headers($prefs["real-name"]);
+			$mail->AddReplyTo($prefs["reply-to"], $UM->mime_encode_headers($prefs["real-name"]));			
+			$mail->AddAddress($recipient);
+			
+			$mail->Subject = $UM->mime_encode_headers(stripslashes($receiptSubj));
+			$mail->Body = stripslashes($receiptText);
+
+			// send
+			if($mail->Send() === true) {
+				echo "success: receipt sent";
+			}			
+			else {
+				echo "error: " . $mail->ErrorInfo;
+			}
+
 			break;
 
+		// just refresh the session timeout
 		case "pingSession":
-			pingSession();	
+			// refresh time
+		        $sess["start"] = time();
+		        // save
+		        $SS->Save($sess);
+
+		        echo "success: session refreshed";
+		
 			break;
+		
 		default:
-			echo "This action does not exist";
+			echo "error: this action does not exist";
 	}
 
 } else {
 	// no action, no fun
-	echo "No action specified";
+	echo "error: no action specified";
 }
-
-/** functions implementations */
-
-// send return receipt
-function sendReceipt() {
-	echo "This action awaits implementation...";	
-}
-
-// Refresh the session, so it remains active 
-function pingSession() {
-	
-	// load session
-	$SS = new Session();
-	$sess = $SS->Load();
-	// refresh time
-	$sess["start"] = time();
-	// save
-	$SS->Save($sess);
-	// just a check for right response
-	if($sess["auth"]) {
-		echo "Ok, session refreshed.";
-	} else {
-		echo "Your session seems expired!";
-	}
-	
-}
-
 
 ?>
--- telaen/redir.php
+++ telaen/redir.php
@@ -1,13 +1,14 @@
-<html>
 <?php
-extract($_SERVER);
+// very bad method to get params... the url is passed without the param name...
+$url = $_SERVER['QUERY_STRING'];
 ?>
+<html>
 <head>
-	<meta http-equiv="refresh" content="0; url=<?php echo $QUERY_STRING; ?>">
+	<meta http-equiv="refresh" content="0; url=<?php echo $url; ?>">
 </head>
 <br>
 <blockquote>
-<a href="<?php echo $QUERY_STRING; ?>"><?php echo $QUERY_STRING; ?></a>
+<a href="<?php echo $url; ?>"><?php echo $url; ?></a>
 </blockquote>
 
 </html>
