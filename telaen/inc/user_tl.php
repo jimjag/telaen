810a7bfd1dc337a0689d5f2af81d3a22270093f9
Jim Jagielski <jimjag@gmail.com>
2009-05-13 19:37:36 +0000

    Detab

--- telaen/inc/user_tl.php
+++ telaen/inc/user_tl.php
@@ -13,37 +13,37 @@ Telaen is a GPL'ed software developed by
  */
 
 if (isset($sess) && is_array($sess) && $sess['thm_lang_inited']) {
-	$tid = $sess['tid'];
-	$lid = $sess['lid'];
-} else {	
-	if(isset($f_pass) && strlen($f_pass) > 0) {
+    $tid = $sess['tid'];
+    $lid = $sess['lid'];
+} else {    
+    if(isset($f_pass) && strlen($f_pass) > 0) {
 
-		if($allow_user_change_theme) {
-			if($tem != "") $tid = $tem;
-			else { $tid = $default_theme; }
-		} else
-			$tid = $default_theme;
+        if($allow_user_change_theme) {
+            if($tem != "") $tid = $tem;
+            else { $tid = $default_theme; }
+        } else
+            $tid = $default_theme;
 
-		if($allow_user_change_language) {
-			if($lng != "") $lid = $lng;
-			else { $lid = $default_language; }
-		} else
-				$lid = $default_language;
-	}
+        if($allow_user_change_language) {
+            if($lng != "") $lid = $lng;
+            else { $lid = $default_language; }
+        } else
+                $lid = $default_language;
+    }
 }
 
 if(!is_numeric($tid) || $tid >= count($themes)) $tid = $default_theme;
 if(!is_numeric($lid) || $lid >= count($languages)) $lid = $default_language;
 
 if (isset($sess) && is_array($sess)) {
-	$sess['thm_lang_inited'] = true;
-	$sess['tid'] = $tid;
-	$sess['lid'] = $lid;
+    $sess['thm_lang_inited'] = true;
+    $sess['tid'] = $tid;
+    $sess['lid'] = $lid;
 }
 
-$selected_theme 	= $themes[$tid]["path"];
+$selected_theme     = $themes[$tid]["path"];
 if (!$selected_theme) die("<br><br><br><div align=center><h3>Invalid theme, configure your \$default_theme</h3></div>");
-$selected_language 	= $languages[$lid]["path"];
+$selected_language  = $languages[$lid]["path"];
 if (!$selected_language) die("<br><br><br><div align=center><h3>Invalid language, configure your \$default_language</h3></div>");
 
 /********************************************************
@@ -71,23 +71,23 @@ $headers_window_template   = "themes/$selected_theme/headers-window.htm";   // J
 $preferences_template      = "themes/$selected_theme/preferences.htm";      // Preferencias
 $adv_editor_template       = "themes/$selected_theme/advanced-editor.htm";  // Advanced HTML Editor
 $catch_address_template    = "themes/$selected_theme/catch-address.htm";    // Address catcher
-$print_message_template		= "themes/$selected_theme/print-message.htm";    // Print friendly version
+$print_message_template     = "themes/$selected_theme/print-message.htm";    // Print friendly version
 
-$menu_template			= "$selected_theme/menu.htm";			// Menu
-$header_template		= "$selected_theme/header.htm";       		// Page Header
-$footer_template        	= "$selected_theme/footer.htm";         	// Page Footer
-$popup_header_template		= "$selected_theme/popup-header.htm";			// Header for the popup wins
+$menu_template          = "$selected_theme/menu.htm";           // Menu
+$header_template        = "$selected_theme/header.htm";             // Page Header
+$footer_template            = "$selected_theme/footer.htm";             // Page Footer
+$popup_header_template      = "$selected_theme/popup-header.htm";           // Header for the popup wins
 
 
 $lg = file("langs/".$selected_language.".txt");
 
 while(list($line,$value) = each($lg)) {
-	if($value[0] == "[") break;
-	if(strpos(";#",$value[0]) === false && ($pos = strpos($value,"=")) != 0 && trim($value) != "") {
-		$varname  = trim(substr($value,0,$pos));
-		$varvalue = trim(substr($value,$pos+1));
-		${$varname} = $varvalue;
-	}
+    if($value[0] == "[") break;
+    if(strpos(";#",$value[0]) === false && ($pos = strpos($value,"=")) != 0 && trim($value) != "") {
+        $varname  = trim(substr($value,0,$pos));
+        $varvalue = trim(substr($value,$pos+1));
+        ${$varname} = $varvalue;
+    }
 }
 
 
