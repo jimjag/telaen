7b00d8096417d02a64ee8a17935d5b5fbe26358c
Jim Jagielski <jimjag@gmail.com>
2009-05-13 19:40:54 +0000

    Detab

--- telaen/folder_list.php
+++ telaen/folder_list.php
@@ -5,34 +5,34 @@ $pcounter = 0;
 $system = Array();
 $personal = Array();
 foreach($folders as $entry) {
-	$entry = $entry["name"];
-	$boxname = $entry;
-	if ($UM->is_system_folder($entry)) {
-		$entry = strtolower($entry);
-		switch ($entry) {
-		case "inbox":
-			$boxname = $inbox_extended;
-			break;
-		case "sent":
-			$boxname = $sent_extended;
-			break;
-		case "trash":
-			$boxname = $trash_extended;
-			break;
-		case "spam":
-			$boxname = ($spam_extended ? $spam_extended : "SPAM");
-			break;
-		}
-		$system[$scounter]["systemname"]	= $entry;
-		$system[$scounter]["name"]		= $boxname;
-		$system[$scounter]["link"]		= "process.php?folder=$entry";
-		$scounter++;
-	} else {
-		$personal[$pcounter]["systemname"]	= $entry;
-		$personal[$pcounter]["name"]		= $boxname;
-		$personal[$pcounter]["link"]		= "process.php?folder=$entry";
-		$pcounter++;
-	}
+    $entry = $entry["name"];
+    $boxname = $entry;
+    if ($UM->is_system_folder($entry)) {
+        $entry = strtolower($entry);
+        switch ($entry) {
+        case "inbox":
+            $boxname = $inbox_extended;
+            break;
+        case "sent":
+            $boxname = $sent_extended;
+            break;
+        case "trash":
+            $boxname = $trash_extended;
+            break;
+        case "spam":
+            $boxname = ($spam_extended ? $spam_extended : "SPAM");
+            break;
+        }
+        $system[$scounter]["systemname"]    = $entry;
+        $system[$scounter]["name"]      = $boxname;
+        $system[$scounter]["link"]      = "process.php?folder=$entry";
+        $scounter++;
+    } else {
+        $personal[$pcounter]["systemname"]  = $entry;
+        $personal[$pcounter]["name"]        = $boxname;
+        $personal[$pcounter]["link"]        = "process.php?folder=$entry";
+        $pcounter++;
+    }
 }
 
 // sort the sys item by sysname so the order will always
