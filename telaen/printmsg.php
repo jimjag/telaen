2f41804b6b1b37bc929a1662fbcf016a8489f05f
Jim Jagielski <jimjag@gmail.com>
2009-05-13 19:41:19 +0000

    Detab

--- telaen/printmsg.php
+++ telaen/printmsg.php
@@ -25,10 +25,10 @@ $body = $email["body"];
 
 //cleanning bad tags to show
 if(eregi("<[ ]*body.*background[ ]*=[ ]*[\"']?([A-Za-z0-9._&?=:/{}%+-]+)[\"']?.*>",$body,$regs))
-	$backimg = 	" background=\"".$regs[1]."\"";
+    $backimg =  " background=\"".$regs[1]."\"";
 $smarty->assign("umBackImg",$backimg);
 if(eregi("<[ ]*body[A-Z0-9._&?=:/\"' -]*bgcolor=[\"']?([A-Z0-9#]+)[\"']?[A-Z0-9._&?=:/\"' -]*>",$body,$regs))
-	$backcolor = " bgcolor=\"".$regs[1]."\"";
+    $backcolor = " bgcolor=\"".$regs[1]."\"";
 $smarty->assign("umBackColor",$backcolor);
 
 $body = eregi_replace("<body","<telaen_body_not_alowed",$body);
@@ -52,26 +52,26 @@ $smarty->assign("umFromList",$ARFrom);
 $ARTo = $email["to"];
 
 for($i=0;$i<count($ARTo);$i++) {
-	$name = $ARTo[$i]["name"];
-	$thismail = $ARTo[$i]["mail"];
-	$link = "newmsg.php?nameto=".urlencode($name)."&mailto=$thismail";
-	$ARTo[$i]["link"] = $link;
-	$ARTo[$i]["title"] = "$name <$thismail>";
-	$smarty->assign("umTOList",$ARTo);
+    $name = $ARTo[$i]["name"];
+    $thismail = $ARTo[$i]["mail"];
+    $link = "newmsg.php?nameto=".urlencode($name)."&mailto=$thismail";
+    $ARTo[$i]["link"] = $link;
+    $ARTo[$i]["title"] = "$name <$thismail>";
+    $smarty->assign("umTOList",$ARTo);
 }
 
 // CC
 $ARCC = $email["cc"];
 if(count($ARCC) > 0) {
-	$smarty->assign("umHaveCC",1);
-	for($i=0;$i<count($ARCC);$i++) {
-		$name = $ARCC[$i]["name"];
-		$thismail = $ARCC[$i]["mail"];
-		$link = "newmsg.php?nameto=".urlencode($name)."&mailto=$thismail";
-		$ARCC[$i]["link"] = $link;
-		$ARCC[$i]["title"] = "$name <$thismail>";
-	}
-	$smarty->assign("umCCList",$ARCC);
+    $smarty->assign("umHaveCC",1);
+    for($i=0;$i<count($ARCC);$i++) {
+        $name = $ARCC[$i]["name"];
+        $thismail = $ARCC[$i]["mail"];
+        $link = "newmsg.php?nameto=".urlencode($name)."&mailto=$thismail";
+        $ARCC[$i]["link"] = $link;
+        $ARCC[$i]["title"] = "$name <$thismail>";
+    }
+    $smarty->assign("umCCList",$ARCC);
 }
 
 $smarty->assign("umPageTitle",$email["subject"]);
@@ -86,16 +86,16 @@ $haveattachs = (count($anexos) > 0)?1:0;
 $smarty->assign("umHaveAttachments",$haveattachs);
 
 if(count($anexos) > 0) {
-	for($i=0;$i<count($anexos);$i++) {
-		$safefname = rawurlencode($anexos[$i]["name"]);
-		$link1 = "download.php?folder=$folder&ix=$ix&mnum=$mnum&bound=".base64_encode($anexos[$i]["boundary"])."&part=".$anexos[$i]["part"]."";
-		$link2 = "$link1&down=1";
-		$anexos[$i]["normlink"] = "$link1&fname=$safefname";
-		$anexos[$i]["downlink"] = "$link2&fname=$safefname";
-		$anexos[$i]["size"] = ceil($anexos[$i]["size"]/1024);
-		$anexos[$i]["type"] = trim($anexos[$i]["content-type"]);
-	}
-	$smarty->assign("umAttachList",$anexos);
+    for($i=0;$i<count($anexos);$i++) {
+        $safefname = rawurlencode($anexos[$i]["name"]);
+        $link1 = "download.php?folder=$folder&ix=$ix&mnum=$mnum&bound=".base64_encode($anexos[$i]["boundary"])."&part=".$anexos[$i]["part"]."";
+        $link2 = "$link1&down=1";
+        $anexos[$i]["normlink"] = "$link1&fname=$safefname";
+        $anexos[$i]["downlink"] = "$link2&fname=$safefname";
+        $anexos[$i]["size"] = ceil($anexos[$i]["size"]/1024);
+        $anexos[$i]["type"] = trim($anexos[$i]["content-type"]);
+    }
+    $smarty->assign("umAttachList",$anexos);
 }
 
 unset($UM);
