a9d8a1b50935ae573acb92177bfaf0a879f6b1a3
Jim Jagielski <jimjag@gmail.com>
2010-05-10 15:46:06 +0000

    If reseting language or theme, we need to keep these...

--- telaen/index.php
+++ telaen/index.php
@@ -12,6 +12,9 @@ require("./inc/lib.php");
 require("./inc/preinit.php");
 require("./inc/user_tl.php");
 
+extract(pull_from_get(Array("f_email", "f_user", "lid", "tid", "six")));
+extract(pull_from_post(Array("f_email", "f_user")));
+
 require_once(SMARTY_DIR."Smarty.class.php");
 $smarty = new Smarty;
 $smarty->security=true;
