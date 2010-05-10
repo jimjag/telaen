74f1ad8476706e75db808cfb18166c8f015010a5
Jim Jagielski <jimjag@gmail.com>
2010-05-10 14:53:39 +0000

    Starting point for embedded calendar

---
+++ telaen/inc/class/class.mymonth.php
@@ -0,0 +1,66 @@
+<?php
+
+/************************************************************************
+Telaen is a GPL'ed software developed by 
+
+ - The Telaen Group
+ - http://www.telaen.com/
+
+*************************************************************************/
+
+class MyMonth {
+	var $_month = 0;
+	var $_year = 0;
+	var $_mymonth;
+	var $_firstday;
+	var $_lastday;
+	var $_today;
+
+	function MyMonth($month=0, $year=0) {
+		if (($month <= 0) || ($month >= 13) || ($year <= 0) || $year >= 2050){
+		   $this->_mymonth	= getdate();
+		   $month = $this->_mymonth['mon'];
+		   $year = $this->_mymonth['year'];
+		} else {
+		   $this->_mymonth	= getdate(mktime(0,0,0,$month,1,$year));
+		}
+		$fom = gmmktime(0,0,0,$this->_mymonth['mon'],1,$this->_mymonth['year']);
+		$wd = explode(',',gmstrftime('%m,%Y,%B,%w',$fom));
+		$this->_firstday = $wd[3];
+		$this->_lastday = gmdate('t',$fom);
+		$this->_today = getdate();
+		$this->_month = intval($month);
+		$this->_year = intval($year);
+	}
+	
+	function showMonth() {
+
+		echo "<table class='month'>";
+		echo "  <tr><th colspan='7'> {$this->_mymonth['month']} - {$this->_mymonth['year']} </th></tr>";
+		echo "  <tr class='days'><td>Su</td><td>Mo</td><td>Tu</td><td>We</td><td>Th</td><td>Fr</td><td>Sa</td></tr>";
+		
+		if (($this->_today['mon'] == $this->_month) && ($this->_today['year'] == $this->_year))
+			$today = $this->_today['mday'];
+		else
+			$today = -1;
+	
+		$weekday = $this->_firstday;
+		echo "<tr>";
+		if($weekday > 0) echo "<td class='regday' colspan='{$weekday}'>&nbsp;</td>";
+		for($day=1; $day<=$this->_lastday; $day++,$weekday++){
+			if($weekday == 7) {
+				$weekday = 0;
+				echo "</tr>\n<tr>";
+			}
+			if ($day == $today) {
+				echo "<td class='today'> $day </td>";
+			} else {
+				echo "<td class='regday'> $day </td>";
+			}
+		}
+		if($weekday != 7) echo "<td class='regday' colspan=".(7-$weekday).">&nbsp;</td>";
+		echo "</tr>";
+		echo "</table>";
+	}
+}
+?>
