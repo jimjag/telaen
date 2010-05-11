ae4795d220e822ef0f8fdb7da07e2a5a134f4013
Jim Jagielski <jimjag@gmail.com>
2010-05-11 13:57:44 +0000

    missed the message index

--- telaen/newmsg.php
+++ telaen/newmsg.php
@@ -14,7 +14,7 @@ $smarty->assign("pageMetas", $nocache);
 
 extract(pull_from_get(Array("nameto", "mailto")));
 extract(pull_from_post(Array("to", "cc", "bcc", "subject", "requireReceipt",
-		"priority", "body", "is_html", "textmode", "sig", "tipo", "rtype")));
+		"priority", "body", "is_html", "textmode", "sig", "tipo", "rtype", "ix")));
 
 if(isset($tipo) && $tipo == "send") {
 
