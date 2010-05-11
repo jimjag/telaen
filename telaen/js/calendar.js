7d9aa444fd2578cf96c81a5e826c1dd35dd408e6
Jim Jagielski <jimjag@gmail.com>
2010-05-11 21:13:52 +0000

    Don't bother with ahrefs

--- telaen/js/calendar.js
+++ telaen/js/calendar.js
@@ -10,7 +10,7 @@ function doDays() {
 	var tds = document.getElementById("calendar").getElementsByTagName("td");
 	for (var i=0; i<tds.length; i++) {
 		if (tds[i].className=="regday" || tds[i].className=="today") {
-			tds[i].getElementsByTagName("a")[0].onclick = function() {
+			tds[i].onclick = function() {
 				var url = "./event.php?idx=" + this.parentNode.id;
 				window.open(url, "Event", "width=550, height=350, scrollbars=1, resizable=1");
 				return false;
