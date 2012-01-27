03f0fc8ff4721d21aca92fcfe0698a58e183b75b
Jim Jagielski <jimjag@gmail.com>
2012-01-27 20:33:17 +0000

    more common js

--- telaen/js/common.js
+++ telaen/js/common.js
@@ -1,16 +1,8 @@
 // common navigation js
-function goinbox() {
-	location = 'messages.php?folder=inbox';
-}
-function addresses() {
-	location = 'addressbook.php';
-}
-function prefs() {
-	location = 'preferences.php';
-}
-function goend() {
-        location = 'logout.php';
-}
+function goinbox()   { window.location = 'messages.php?folder=inbox'; }
+function addresses() { window.location = 'addressbook.php'; }
+function prefs()     { window.location = 'preferences.php'; }
+function goend()     { window.location = 'logout.php'; }
 
 /**
  * This set an action (if needed) and submit the form
@@ -72,5 +64,53 @@ function selectAll(formid, n) {
 			cb.checked = ck;
 		}
 	}
+}
+
+/**
+ * clock funcs
+ */
+var timerID = null;
+var timerRunning = false;
+
+function stopclock() {
+if(timerRunning)
+	clearTimeout(timerID);
+	timerRunning = false;
+}
+function showtime() {
+	var now = new Date();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+	var timeValue = "" + ((hours >12) ? hours -12 :hours)
+	if (timeValue == "0"){timeValue = 12;}
+	timeValue += ((minutes < 10) ? ":0" : ":") + minutes
+	timeValue += ((seconds < 10) ? ":0" : ":") + seconds
+	timeValue += (hours >= 12) ? " PM" : " AM"
+	document.getElementById('clock').innerHTML = timeValue;
+
+	// catch midnight rollover and refresh date
+	if(timeValue == '0:00:01 AM'){makeDate();}
+
+	timerID = setTimeout("showtime()",1000);
+	timerRunning = true;
+}
+function makeDate() {
+	var mydate=new Date();
+	var year=mydate.getYear();
+	if(year < 1000){year+=1900;}
+	var day=mydate.getDay();
+	var month=mydate.getMonth();
+	var daym=mydate.getDate();
+	if (daym<10){daym="0"+daym;}
+	var dayarray=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+	var montharray=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+	var datexx = dayarray[day]+", "+montharray[month]+" "+daym+", "+year;
+	document.getElementById('dfield').innerHTML = datexx;
+}
+function startclock() {
+	stopclock();
+	showtime();
+	makeDate();
 }
 
