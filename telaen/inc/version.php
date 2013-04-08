5c06393673c640e9260f05ed03c1b948050b7672
Jim Jagielski <jimjag@gmail.com>
2013-04-08 14:59:27 +0000

    and we are in dev

--- telaen/inc/version.php
+++ telaen/inc/version.php
@@ -12,7 +12,7 @@ Telaen is a GPL'ed software developed by
 # "X-Mailer" field, footer
 ########################################################################
 
-$appversion = "1.3.1";
+$appversion = "1.3.2-dev";
 $appname = "Telaen Webmail";
 
 ?>
