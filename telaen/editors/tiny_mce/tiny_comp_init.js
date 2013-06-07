882f457804fa8733c3c15ac48690bb60b5951aea
Jim Jagielski <jim@jaguNET.com>
2013-06-07 16:48:35 -0400

    New files to be added

---
+++ telaen/editors/tiny_mce/tiny_comp_init.js
@@ -0,0 +1,9 @@
+// compressor init
+tinyMCE_GZ.init({
+        plugins : 'table,advimage,advlink,iespell,insertdatetime,preview,flash,searchreplace,print,contextmenu,paste,directionality,fullscreen',
+        themes : 'simple,advanced',
+        languages : 'en',
+        disk_cache : true,
+        debug : false
+});
+
