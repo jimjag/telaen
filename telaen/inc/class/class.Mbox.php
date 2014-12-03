c2191d574081031e20153019d74e3ae13754d04c
Jim Jagielski <jimjag@gmail.com>
2014-12-03 17:07:33 -0500

    move to using sqlite3, which is default in PHP 5.4+ AND is quicker than using arrays and serialize/etc

--- telaen/inc/class/class.Mbox.php
+++ telaen/inc/class/class.Mbox.php
@@ -8,46 +8,83 @@ Telaen is a GPL'ed software developed by
 *************************************************************************/
 
 /**
- * Simple PHP Helper for file-based Email data
+ * Simple PHP Helper for sqlite3-based Email data
  */
-class Mbox
+class Mbox extends SQLite3
 {
-    private $path = "";
-
-    public function __construct()
-    {
-    }
-
-    /**
-     * Load in Array from file
-     * @param  string $path File to read
-     * @return array
-     */
-    public function &Load($path)
-    {
-        $this->path = $path;
-        $ret = array();
-        if ($this->path) {
-            $str = @file_get_contents($this->path);
-            if ($str) {
-                $ret = unserialize($str);
-            }
-        }
-
-        return $ret;
-    }
+    private $db = null;
+    private $table_headers =<<<EOF_HEADERS
+        CREATE TABLE headers
+        (crc32 INT NOT NULL,
+        folder TEXT NOT NULL,
+        headers TEXT NOT NULL);
+EOF_HEADERS;
+    private $table_folders =<<<EOF_FOLDERS
+        CREATE TABLE folders
+        (crc32 INT NOT NULL,
+        folder TEXT NOT NULL);
+EOF_FOLDERS;
+    private $table_attachs =<<<EOF_ATTACHS
+        CREATE TABLE attachs
+        (crc32 INT NOT NULL,
+        folder TEXT NOT NULL,
+        localname TEXT,
+        name TEXT,
+        type TEXT,
+        size TEXT);
+EOF_ATTACHS;
 
     /**
-     * Save array to file
-     * @param  array $array2save Array to tuck away
-     * @return void
+     * Construct: open DB and create tables if needed
+     * @param type $userfolder
      */
-    public function Save(&$array2save)
+    public function __construct($userfolder)
     {
-        $f = fopen($this->path, 'w');
-        if ($f) {
-            fwrite($f, serialize($array2save));
-            fclose($f);
+        $this->db = $userfolder.'_infos/mboxes.db';
+        $this->open($this->db, SQLITE3_OPEN_READWRITE| SQLITE3_OPEN_CREATE);
+        $try =  $this->query("SELECT name FROM sqlite_master WHERE type='table' AND name='folders';");
+        if ($try->numColumns == 0) {
+            $this->exec($this->table_folders);
+            $this->exec($this->table_headers);
+            $this->exec($this->table_attachs);
         }
     }
+
+
+    public function headers($id)
+    {
+        $stmt = $this->prepare('SELECT * FROM headers WHERE crc32=:id');
+        $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        $rarr = array();
+        while ($foo = $result->fetchArray()) {
+            $rarr[] = $foo;
+        }
+        $stmt->close();
+        return $rarr;
+    }
+
+    public function attachs($id)
+    {
+        $stmt = $this->prepare('SELECT * FROM attachs WHERE crc32=:id');
+        $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        $rarr = array();
+        while ($foo = $result->fetchArray()) {
+            $rarr[] = $foo;
+        }
+        $stmt->close();
+        return $rarr;
+    }
+
+    public function folders()
+    {
+        $stmt = $this->query('SELECT * FROM folders');
+        $rarr = array();
+        while ($foo = $stmt->fetchArray()) {
+            $rarr[] = $foo;
+        }
+        return $rarr;
+    }
+
 }
