0fc74c8505c5900ea9e7b79b01c7e0ff30dfc1c3
Jim Jagielski <jimjag@gmail.com>
2015-01-09 09:28:25 -0500

    typo

--- telaen/redir.php
+++ telaen/redir.php
@@ -1,7 +1,7 @@
 <?php
 define('I_AM_TELAEN', basename($_SERVER['SCRIPT_NAME']));
 
-require './inc/class/class.Telain_core.php';
+require './inc/class/class.Telaen_core.php';
 
 // very bad method to get params... the url is passed without the param name...
 $url = Telaen_core::create_abs_url($_SERVER['QUERY_STRING']);
