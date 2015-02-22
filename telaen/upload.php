47422013ec764ed60702463d33fecab217a1e7d2
Jim Jagielski <jimjag@gmail.com>
2015-02-22 15:21:47 -0500

    use arra

--- telaen/upload.php
+++ telaen/upload.php
@@ -56,7 +56,7 @@ $attch = $TLN->tdb->getAttachments(['uidl' => 'tmp', 'folder' => 'tmp']);
 foreach ($attch as $a) {
     echo "
     <tr>
-     <td width=\"50%\" class=\"default\">".urlencode(['name'])."</td>
+     <td width=\"50%\" class=\"default\">".urlencode($a['name'])."</td>
      <td width=\"10%\" class=\"right\">".Telaen::bytes2bkmg($a['size'])."</td>
      <td width=\"30%\" class=\"default\">".$a['type'].'/'.$a['subtype']."</td>
      <td width=\"10%\" class=\"default\"><a href=\"javascript:delatt(".urlencode($a['name']).")\">OK</a></td>
